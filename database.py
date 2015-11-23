a2bfd4c45c6c1aeeb88ed9666ba6c1fd5b5b69dc
Brandon Tan <http://tanb@git.socs.uoguelph.ca>
2015-11-23 15:57:01 -0500

    SQL init working

--- database.py
+++ database.py
@@ -6,15 +6,15 @@ import host_site
 #passwd="DenotedSundials", # your password
 #db="PositiveFish") # name of the data base
 
-db = MySQLdb.connect(host="206.248.176.247:3306", # our host, do not modify
+db = MySQLdb.connect(host="206.248.176.247", # our host, do not modify
                      user="root", # your username
                      passwd="taco#taco", # your password
                      db="visdb") # name of the data base
 
 initCur = db.cursor()
-initCur.execute("CREATE TABLE IF NOT EXISTS users {username VARCHAR(40), password VARCHAR(40), firstname VARCHAR(40), lastname VARCHAR(40), email VARCHAR(80), teamcaptain BOOLEAN, accessible BOOLEAN}")
-initCur.execute("CREATE TABLE IF NOT EXISTS routes {name VARCHAR(40), lattitudeStart FLOAT, longitudeStart FLOAT, lattitudeEnd FLOAT, longitudeEnd FLOAT, accessible BOOLEAN, transport VARCHAR(40)}")
-initCur.execute("CREATE TABLE IF NOT EXISTS users {docname VARCHAR(40), contents VARCHAR(8000)}")
+initCur.execute("CREATE TABLE IF NOT EXISTS users (username VARCHAR(40), password VARCHAR(40), firstname VARCHAR(40), lastname VARCHAR(40), email VARCHAR(80), teamcaptain BOOLEAN, accessibilityNeeds BOOLEAN)")
+initCur.execute("CREATE TABLE IF NOT EXISTS routes (name VARCHAR(40), lattitudeStart FLOAT, longitudeStart FLOAT, lattitudeEnd FLOAT, longitudeEnd FLOAT, isAccessible BOOLEAN, transport VARCHAR(40))")
+initCur.execute("CREATE TABLE IF NOT EXISTS faq (question VARCHAR(8000), answer VARCHAR(8000))")
 initCur.close()
 
 
